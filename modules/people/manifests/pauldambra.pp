a9433bb18888e37f71c0e68d6ee5ea4a035d09d8
Paul D'Ambra <paul.dambra@gmail.com>
2014-06-11 16:27:37 +0100

    adding personalised settings for pauldambra

---
+++ modules/people/manifests/pauldambra.pp
@@ -0,0 +1,34 @@
+class people::pauldambra {
+	include chrome
+	include virtualbox
+	include alfred
+	include firefox
+	include skype
+	include dropbox
+	include vagrant
+	include googledrive
+
+	
+	include osx::global::tap_to_click
+	include osx::dock::autohide
+	include osx::software_update
+
+	class { 'osx::dock::position':
+	  position => 'left'
+	}
+
+	include sublime_text
+
+	sublime_text::package { 'Emmet':
+	  source => 'sergeche/emmet-sublime'
+	}
+
+	sublime_text::package { 'JSHint Gutter': 
+	  source => 'victorporof/Sublime-JSHint'
+	}
+
+	class { 'vagrant':
+	  completion => true,
+	}
+}
+
